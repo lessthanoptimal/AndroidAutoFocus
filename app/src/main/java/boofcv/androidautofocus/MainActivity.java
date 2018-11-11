16ddaaac608ad778c1b7c6a52fe8085063cfe172
Peter Abeles <peter.abeles@gmail.com>
2018-11-11 12:38:04 -0800

    - comments and formatting

--- app/src/main/java/boofcv/androidautofocus/MainActivity.java
+++ app/src/main/java/boofcv/androidautofocus/MainActivity.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (c) 2011-2013, Peter Abeles. All Rights Reserved.
- *
- * This file is part of BoofCV (http://boofcv.org).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package boofcv.androidautofocus;
 
 import android.Manifest;
@@ -129,21 +111,25 @@ public class MainActivity extends VisualizeCamera2Activity
 
 	@Override
 	protected void processImage(ImageBase image) {
+		// We specified earlier to give us an 8-bit gray scale image
 		GrayU8 gray = (GrayU8)image;
+
+		// Ensure that work space images are the appropriate size
 		derivX.reshape(gray.width,gray.height);
 		derivY.reshape(gray.width,gray.height);
 		intensity.reshape(gray.width,gray.height);
-		GImageDerivativeOps.gradient(DerivativeType.SOBEL,gray,derivX,derivY, BorderType.EXTENDED);
 
+		// Compute the gradient and Euclidean edge intensity
+		GImageDerivativeOps.gradient(DerivativeType.SOBEL,gray,derivX,derivY, BorderType.EXTENDED);
 		GGradientToEdgeFeatures.intensityE(derivX, derivY, intensity);
 
+		// Find the average edge value
 		edgeValue = ImageStatistics.mean(intensity);
 	}
 
 	@Override
 	protected void onDrawFrame(SurfaceView view, Canvas canvas) {
 		super.onDrawFrame(view, canvas);
-
 
 		// Darken the image behind the text to make it easier to read
 		canvas.drawRect(0,100,view.getRight(),220,paintGray);
@@ -232,7 +218,6 @@ public class MainActivity extends VisualizeCamera2Activity
 
 	@Override
 	public void onClick(View v) {
-
 		// If the user touches the screen and it has already finished focusing, start again
 		if( state == State.FIXED ) {
 			state = State.INITIALIZE;
